4555abc19d4fee2822968c63c0d3025367c46585
Prog84 <bvv198424@rambler.ru>
2020-10-09 15:46:25 +1000

    create scripts: heart, healthbar, parallax, gameoverscreen

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : ObjectPool
+{
+    [SerializeField] private GameObject[] _enemyTmplates;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _elapsedTime = 0;
+
+    private void Start()
+    {
+        Initialize(_enemyTmplates);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elapsedTime = 0;
+
+                int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+
+                SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+            }
+        }
+    }
+
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint;
+    }
+}
